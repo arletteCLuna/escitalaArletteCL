ab0118addc3c5b26f33ac93aca49e84fbf9b5b76
arletteCLuna <128724916+arletteCLuna@users.noreply.github.com>
2023-10-18 21:35:52 -0600

    funciones.js

--- funciones.js
+++ funciones.js
@@ -1,37 +1,90 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const inputText = document.getElementById("inputText");
-    const shift = document.getElementById("shift");
-    const encryptButton = document.getElementById("encryptButton");
-    const decryptButton = document.getElementById("decryptButton");
-    const result = document.getElementById("result");
-  
-    encryptButton.addEventListener("click", function () {
-      result.textContent = transpositionCipher(inputText.value, parseInt(shift.value));
-    });
-  
-    decryptButton.addEventListener("click", function () {
-      result.textContent = transpositionDecipher(inputText.value, parseInt(shift.value));
-    });
-  
-    function transpositionCipher(text, shift) {
-      const textArray = text.split("");
-      const encryptedArray = textArray.map((char, index) => {
-        const newIndex = (index + shift) % textArray.length;
-        return textArray[newIndex];
-      });
-      return encryptedArray.join("");
-    }
-  
-    function transpositionDecipher(text, shift) {
-      const textArray = text.split("");
-      const decryptedArray = textArray.map((char, index) => {
-        let newIndex = (index - shift) % textArray.length;
-        if (newIndex < 0) {
-          newIndex = textArray.length + newIndex;
-        }
-        return textArray[newIndex];
-      });
-      return decryptedArray.join("");
-    }
-  });
+function calcularCaracteres() {
+  const lados = parseInt(document.getElementById("lados").value);
+  const vueltas = parseInt(document.getElementById("vueltas").value);
+
+  if (isNaN(lados) || isNaN(vueltas)) {
+      alert("Por favor, ingresa números enteros válidos.");
+      return;
+  }
+
+  const caracteresPermitidos = lados * vueltas;
+  document.getElementById("caracteresPermitidos").textContent = caracteresPermitidos;
+  document.getElementById("caracteresPermitidosLabel").textContent = caracteresPermitidos;
+
+  // Habilitar el campo de texto del mensaje y establecer la longitud máxima
+  document.getElementById("mensaje").disabled = false;
+  document.getElementById("mensaje").maxLength = caracteresPermitidos;
+}
+
+function cifrarMensaje() {
+  const mensaje = document.getElementById("mensaje").value;
+  const lados = parseInt(document.getElementById("lados").value);
+
+  if (mensaje.length === 0) {
+      alert("Por favor, ingresa un mensaje.");
+      return;
+  }
+
+  const mensajeCifrado = escitalaCifrar(mensaje, lados);
+  document.getElementById("resultado").textContent = mensajeCifrado;
+}
+
+function descifrarMensaje() {
+  const mensajeCifrado = document.getElementById("mensaje").value;
+  const lados = parseInt(document.getElementById("lados").value);
+
+  if (mensajeCifrado.length === 0) {
+      alert("Por favor, ingresa un mensaje cifrado.");
+      return;
+  }
+
+  const mensajeDescifrado = escitalaDescifrar(mensajeCifrado, lados);
+  document.getElementById("resultado").textContent = mensajeDescifrado;
+}
+
+function escitalaCifrar(mensaje, lados) {
+  const caracteresPorColumna = Math.ceil(mensaje.length / lados);
+  const mensajeCifrado = [];
+
+  for (let i = 0; i < caracteresPorColumna; i++) {
+      for (let j = i; j < mensaje.length; j += caracteresPorColumna) {
+          mensajeCifrado.push(mensaje[j]);
+      }
+  }
+
+  return mensajeCifrado.join(''); // Devolver como una cadena
+}
+
+function escitalaDescifrar(mensajeCifrado, lados) {
+  const caracteresPorColumna = Math.ceil(mensajeCifrado.length / lados);
+  const filas = lados;
+  const columnas = caracteresPorColumna;
+
+  let k = 0;
+  const matriz = new Array(filas);
+
+  for (let i = 0; i < filas; i++) {
+      matriz[i] = new Array(columnas);
+  }
+
+  for (let j = 0; j < columnas; j++) {
+      for (let i = 0; i < filas; i++) {
+          if (k < mensajeCifrado.length) {
+              matriz[i][j] = mensajeCifrado[k];
+              k++;
+          }
+      }
+  }
+
+  const mensajeDescifrado = [];
+  for (let i = 0; i < filas; i++) {
+      for (let j = 0; j < columnas; j++) {
+          if (matriz[i][j]) {
+              mensajeDescifrado.push(matriz[i][j]);
+          }
+      }
+  }
+
+  return mensajeDescifrado.join(''); 
+}
   
